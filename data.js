c77dd9df38f36a9f866712f488d8774782fa647a
detectivecrewze <aldoramadhan16@gmail.com>
2026-04-07 11:04:02 +0700

    Update Valentine data manually

--- data.js
+++ data.js
@@ -9,23 +9,51 @@ window.VALENTINE_DATA = {
     "particles": "hearts"
   },
   "login": {
-    "password": "123",
-    "instruction": "Tanggal jadian kita?"
+    "password": "sushi",
+    "instruction": "My favorite food?"
   },
   "music": [
     {
       "type": "mp3",
       "isLibrary": true,
-      "url": "https://arcade-edition.aldoramadhan16.workers.dev/files/1774159153442-rdfg39.mp3",
-      "name": "Segalanya Itu Kamu",
-      "coverUrl": "https://arcade-edition.aldoramadhan16.workers.dev/files/1774159199143-o5biiq.jpg",
-      "title": "Segalanya Itu Kamu",
-      "artist": "David Bayu",
-      "quotes": "♪ Kau yang merajai hatiku — dan kau segalanya, segalanya itu kamu ♪",
-      "songTitle": "Segalanya Itu Kamu",
-      "audioSrc": "https://arcade-edition.aldoramadhan16.workers.dev/files/1774159153442-rdfg39.mp3",
-      "coverSrc": "https://arcade-edition.aldoramadhan16.workers.dev/files/1774159199143-o5biiq.jpg",
-      "lyrics": "♪ Kau yang merajai hatiku — dan kau segalanya, segalanya itu kamu ♪"
+      "url": "https://arcade-edition.aldoramadhan16.workers.dev/files/1773904097278-bqxgv.mp3",
+      "name": "Baby I'm Yours",
+      "coverUrl": "https://arcade-edition.aldoramadhan16.workers.dev/files/1773904167183-o29nvq.jpg",
+      "title": "Baby I'm Yours",
+      "artist": "Artic Monkeys",
+      "quotes": "♪ Baby, I'm yours — and I'll be yours until the stars fall from the sky ♪",
+      "songTitle": "Baby I'm Yours",
+      "audioSrc": "https://arcade-edition.aldoramadhan16.workers.dev/files/1773904097278-bqxgv.mp3",
+      "coverSrc": "https://arcade-edition.aldoramadhan16.workers.dev/files/1773904167183-o29nvq.jpg",
+      "lyrics": "♪ Baby, I'm yours — and I'll be yours until the stars fall from the sky ♪"
+    },
+    {
+      "type": "mp3",
+      "isLibrary": true,
+      "url": "https://arcade-edition.aldoramadhan16.workers.dev/files/1773904364430-4b7hn9.mp3",
+      "name": "About You",
+      "coverUrl": "https://arcade-edition.aldoramadhan16.workers.dev/files/1773904442555-tjdyhk.jpg",
+      "title": "About You",
+      "artist": "The 1975",
+      "quotes": "♪ Do you think I have forgotten? — I could lay and just look in your eyes ♪",
+      "songTitle": "About You",
+      "audioSrc": "https://arcade-edition.aldoramadhan16.workers.dev/files/1773904364430-4b7hn9.mp3",
+      "coverSrc": "https://arcade-edition.aldoramadhan16.workers.dev/files/1773904442555-tjdyhk.jpg",
+      "lyrics": "♪ Do you think I have forgotten? — I could lay and just look in your eyes ♪"
+    },
+    {
+      "type": "mp3",
+      "isLibrary": true,
+      "url": "https://arcade-edition.aldoramadhan16.workers.dev/files/1774844574937-tfx2t.mp3",
+      "name": "Bad.",
+      "coverUrl": "https://arcade-edition.aldoramadhan16.workers.dev/files/1774466444707-y7abub.jpg",
+      "title": "Bad.",
+      "artist": "Wave To Earth",
+      "quotes": "♪ How could my day be bad — when I'm with you? ♪",
+      "songTitle": "Bad.",
+      "audioSrc": "https://arcade-edition.aldoramadhan16.workers.dev/files/1774844574937-tfx2t.mp3",
+      "coverSrc": "https://arcade-edition.aldoramadhan16.workers.dev/files/1774466444707-y7abub.jpg",
+      "lyrics": "♪ How could my day be bad — when I'm with you? ♪"
     }
   ],
   "musicSectionTitle": "Our Playlist",
@@ -34,9 +62,51 @@ window.VALENTINE_DATA = {
     "subtitle": "Scratch untuk reveal",
     "memories": [
       {
+        "type": "video",
+        "src": "https://arcade-assets.for-you-always.my.id/loves/1775475287776-uclgdao.mp4",
+        "caption": "Everything feels right when i'm with you.",
+        "tape": "washi-tape",
+        "rotation": "rotate-2"
+      },
+      {
         "type": "image",
-        "src": "https://loves-edition.aldoramadhan16.workers.dev/files/loves/1775355325001-gk7l3hk.jpeg",
-        "caption": "Hello, im in here. ",
+        "src": "https://arcade-assets.for-you-always.my.id/loves/1775472626887-w6huz3z.jpg",
+        "caption": "Our first Museum Date",
+        "tape": "washi-tape",
+        "rotation": "rotate-2"
+      },
+      {
+        "type": "image",
+        "src": "https://arcade-assets.for-you-always.my.id/loves/1775475851161-de6mzzg.jpg",
+        "caption": "Our first trip ever!",
+        "tape": "washi-tape",
+        "rotation": "rotate-2"
+      },
+      {
+        "type": "video",
+        "src": "https://arcade-assets.for-you-always.my.id/loves/1775476230539-qf1gwk8.mp4",
+        "caption": "Missed this moments with u.",
+        "tape": "washi-tape",
+        "rotation": "rotate-2"
+      },
+      {
+        "type": "image",
+        "src": "https://arcade-assets.for-you-always.my.id/loves/1775475850606-1l03kko.jpg",
+        "caption": "Our first date, you looks so good on that outfit.",
+        "tape": "washi-tape",
+        "rotation": "rotate-2"
+      },
+      {
+        "type": "video",
+        "src": "https://arcade-assets.for-you-always.my.id/loves/1775475672535-zhtp2an.mp4",
+        "caption": "With you, there’s no such thing as a boring day.",
+        "tape": "washi-tape",
+        "rotation": "rotate-2"
+      },
+      {
+        "type": "video",
+        "src": "https://arcade-assets.for-you-always.my.id/loves/1775526917489-u533gyy.mov",
+        "caption": "",
         "tape": "washi-tape",
         "rotation": "rotate-2"
       }
@@ -46,28 +116,33 @@ window.VALENTINE_DATA = {
     "vibeLabel": "Our Vibe",
     "vibe": "Beauty & The Beast",
     "HoursTogetherLabel": "Hours Together",
-    "HoursTogether": "2025-11-19",
+    "HoursTogether": "2022-04-24",
     "imageSrc": "https://arcade-assets.for-you-always.my.id/loves/1775442598985-zan9q7l.jpg",
     "topPlacesLabel": "Top Places",
     "topPlaces": [
-      "Labuan Bajo"
+      "Blok M",
+      "Dream Dates",
+      "Stone Garden",
+      "Tebet Eco Park"
     ],
     "coreMemoriesLabel": "Top Song",
     "coreMemories": [
-      "Risk it all - Bruno Mars"
+      "Risk it all - Bruno Mars",
+      "Arctic Monkeys - Baby I'm Yours",
+      "True Love Waits - Radiohead"
     ]
   },
   "letter": {
     "recipient": "Dearest Lisa",
-    "message": "Lisa, aku suka banget ngeliat kamu senyum, apalagi pas kamu lagi semangat ngejar impian atau sekadar nyari kopi favorit di warung deket kos. Kamu itu orangnya sabar, lucu tanpa harus paksain, dan selalu bikin hari-hari aku jadi lebih ringan—bukan karena kamu sempurna, tapi karena kamu bener-bener *kamu*. Aku bangga sama semua usaha kecil yang kamu lakuin tiap hari, dan aku janji bakal terus di sampingmu, bukan cuma pas lagi happy, tapi juga pas lagi lelah atau ragu. Makasih.",
+    "message": "Hey Lisa, thinking of you just made me smile. I love how authentic and kind you are, and how you always know exactly what I need. You make every day better. I’m not trying to be perfect, I just want to always show up, listen, and cheer you on. Thanks for being mine. I really, really like YOU. ",
     "signature": "Your Biggest Fans."
   },
   "invitation": {
     "question": "Would you like to be my one and only one?",
-    "successMessage": "Yay!"
+    "successMessage": "Yay! Thank you for everything, Babe ❤️"
   },
   "metadata": {
-    "requestDomain": "test",
-    "requestedAt": "2026-04-06T08:52:47.403Z"
+    "requestDomain": "sad",
+    "requestedAt": "2026-04-07T03:16:12.811Z"
   }
 };
